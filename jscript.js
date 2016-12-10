94e7bcc5df5e328dcf56f747b362fadad0b2fac5
Abbatchi <abbatchi@gmail.com>
2016-12-10 22:34:35 +0100

    exo3
    exo3

--- jscript.js
+++ jscript.js
@@ -2,3 +2,5 @@ var nom = "John";
 var prenom = "Doe";
 var city = "Ney York";
 alert("       Nom: "+ nom + "\n       Prenom: " + prenom + "\n       Ville: " + city);
+var newName = prompt("Votre nom?");
+alert("Bonjour, " + newName + "!");
